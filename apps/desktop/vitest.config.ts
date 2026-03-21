fed5cfd9bd00f361ac57c83fee38d18ef11d9e34
Patrick Roelofs <contact@patrickroelofs.com>
2026-03-21 20:04:28 +0100

    Revert "init tauri"
    This reverts commit 745a7fbde9a51efa1bed11b942f9cea21dfdb592.

--- apps/desktop/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import viteTsConfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-	plugins: [viteTsConfigPaths({ projects: ["./tsconfig.json"] })],
-	test: {
-		environment: "jsdom",
-	},
-});
